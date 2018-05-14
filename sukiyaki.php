699a409be286fb93b3db1fad08fb78217d4e2375
Iyra <iyra72@gmail.com>
2018-05-14 02:06:45 +0100

    gpl conformance

--- sukiyaki.php
+++ sukiyaki.php
@@ -1,8 +1,18 @@
 <?php
 /* sukiyaki - a PHP futaba-style imageboard.
-Copyright (C) iyra
-License: CC0 1.0 Universal Public Domain Dedication
-https://creativecommons.org/publicdomain/zero/1.0/
+    Copyright (C) 2017, 2018 Iyra Gaura
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 protip: you can use it as a blog too!
 ADMINOPONLY=1,
